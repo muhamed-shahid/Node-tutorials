402711f839b28ea21aa8e48229be5558b26f3e5d
muhamed-shahid <muhamedshahid7860@gmail.com>
2025-11-06 16:17:49 +0530

    use of get method

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express")
-const dotenv = require('dotenv')
+const dotenv = require('dotenv').config()
 const app = express()
 
 app.listen(process.env.PORT,()=>{
     console.log("server running")
+})
+
+app.get('/',(req,res)=>{
+    res.send("welcome all")
 })
