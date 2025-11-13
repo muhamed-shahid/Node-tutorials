ded771d6cbee5fef5e6deeef79980d0c4c59d01b
muhamed-shahid <muhamedshahid7860@gmail.com>
2025-11-13 13:32:49 +0530

    how to fetch data from another file

---
+++ data.js
@@ -0,0 +1,23 @@
+ const products =[
+    {
+        id:1,
+        name:"hello",
+        age:23,
+    },
+    {
+        id:2,
+        name:"welcome",
+        age:23,
+    },
+    {
+        id:3,
+        name:"hyyy",
+        age:23,
+
+
+
+    },
+];
+
+
+module.exports={products};
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const dotenv = require('dotenv').config()
+const products=require('./data.js')
 const app = express()
+
+app.get('/',(req,res)=>{
+    res.json(products)
+})
 
 app.listen(process.env.PORT,()=>{
     console.log("server running")
 })
 
-app.get('/',(req,res)=>{
-    res.send("welcome all")
-})
