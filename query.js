c869273cdc537566b1e0c462735342edeacaca15
muhamed-shahid <muhamedshahid7860@gmail.com>
2025-11-18 13:09:33 +0530

    How to serch using queries

---
+++ query.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv').config()
+
+app.get('/search',(req,res)=>{
+    const keyword = req.query.keyword
+   
+    if(!keyword){
+      return  res.send('Enter a keyword')
+    }
+    else{
+      return  res.send(`You searched for "${keyword}" `)
+    }
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log('server running in', process.env.PORT);
+    
+
+})
