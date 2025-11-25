d78e80fe21536c4999563936728d2f5cf7a0f29d
muhamed-shahid <muhamedshahid7860@gmail.com>
2025-11-25 12:32:44 +0530

    Middleware creation in another page

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require("express")
 const dotenv = require('dotenv').config()
 const products=require('./data.js')
+const logger=require('./logger.js')
 const app = express()
 
 
-// Declaration of middleware in same page
 
-const logger=(req,res,next)=>{
-    console.log("middleware");
-    next()
-    
-}
+
 
 app.get('/',logger,(req,res)=>{
     res.send("HOME PAGEEE")
